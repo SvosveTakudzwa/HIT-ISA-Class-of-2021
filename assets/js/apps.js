7c6a6098f1e2235aebc56808b85c1a8ff58287cd
SvosveTakudzwa <admiretakudzwasvosve@gmail.com>
2024-05-10 09:24:54 +0200

    Initial commit

---
+++ assets/js/apps.js
@@ -0,0 +1,7 @@
+//alert("Hello Viewer. Welcome to the Official Web Technologies ISA Webpage class of 2022");
+
+$(function(){
+	$(".btn").click(function(){
+		$(".submenu").slideToggle(500);
+	});
+});  
